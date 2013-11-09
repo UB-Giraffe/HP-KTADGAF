466b23010a4002a21ea9eb00c28009297280c9f0
jjgregor <jjgregor@buffalo.edu>
2013-11-08 22:09:14 -0500

    Updated manifest

--- SafeTexting/gen/com/example/safetexting/R.java
+++ SafeTexting/gen/com/example/safetexting/R.java
@@ -29,6 +29,7 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main1=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
